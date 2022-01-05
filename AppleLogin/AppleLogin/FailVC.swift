795b260ee133194265189293fe54d4ae0b0a38f0
hellozo0 <ahju0011@gmail.com>
2022-01-05 22:43:49 +0900

    ➕[ADD] : 성공할 경우, 실패할 경우 (#7)

---
+++ AppleLogin/AppleLogin/FailVC.swift
@@ -0,0 +1,23 @@
+//
+//  FailVC.swift
+//  AppleLogin
+//
+//  Created by 안현주 on 2022/01/05.
+//
+
+import UIKit
+
+class FailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpToGoBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+
+    }
+ 
+}
---
+++ AppleLogin/AppleLogin/SuccessVC.swift
@@ -0,0 +1,21 @@
+//
+//  SuccessVC.swift
+//  AppleLogin
+//
+//  Created by 안현주 on 2022/01/05.
+//
+
+import UIKit
+
+class SuccessVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func touchUpToGoBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
