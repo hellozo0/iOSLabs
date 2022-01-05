a52d7c6fc5185a439b7f8d11ea0528888e9e5cc5
hellozo0 <ahju0011@gmail.com>
2022-01-05 17:01:07 +0900

    ✅[CHORE] : 카카오 로그인 import (#5)

--- KakaoLogin/KakaoLogin/AppDelegate.swift
+++ KakaoLogin/KakaoLogin/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDK.initSDK(appKey: "fed7b0affde28bf6bca484625480a339")
+        
         return true
     }
 
