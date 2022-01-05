e80031b930a7b8061504c094e16f9b847d292870
hellozo0 <ahju0011@gmail.com>
2022-01-05 17:02:01 +0900

    ✅[CHORE] : 성공, 실패시 화면 이동 (#5)

---
+++ KakaoLogin/KakaoLogin/SuccessVC.swift
@@ -0,0 +1,32 @@
+//
+//  SuccessVC.swift
+//  KakaoLogin
+//
+//  Created by 안현주 on 2022/01/05.
+//
+
+import UIKit
+
+class SuccessVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpToGoBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
