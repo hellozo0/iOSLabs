ccc5c3f3ae77a2fda4a58cafbad7343d9e834321
hellozo0 <ahju0011@gmail.com>
2022-01-09 23:23:32 +0900

    ✅[CHORE] : Xib 세팅 (#10)

--- TestCarousel/TestCarousel/CarouselCVC.swift
+++ TestCarousel/TestCarousel/CarouselCVC.swift
@@ -22,3 +22,28 @@ class CarouselCVC: UICollectionViewCell {
     }
 
 }
+
+//let customView: UIImageView = {
+//       let view = UIImageView()
+//       view.translatesAutoresizingMaskIntoConstraints = false
+//       view.layer.cornerRadius = 2
+//       view.clipsToBounds = true
+//
+//       return view
+//   }()
+//
+//   override init(frame: CGRect) {
+//       super.init(frame: frame)
+//
+//       self.addSubview(self.customView)
+//
+//       self.customView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+//       self.customView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+//       self.customView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1).isActive = true
+//       self.customView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1).isActive = true
+//   }
+//
+//   required init?(coder aDecoder: NSCoder) {
+//       fatalError("init(coder:) has not been implemented")
+//   }
+//}
