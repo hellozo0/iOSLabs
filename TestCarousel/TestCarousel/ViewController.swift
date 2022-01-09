08a5a2c8838425c01fa56ee47211b3313e8c5bb1
hellozo0 <ahju0011@gmail.com>
2022-01-09 23:23:14 +0900

    ✅[CHORE] : VC (#10)

--- TestCarousel/TestCarousel/ViewController.swift
+++ TestCarousel/TestCarousel/ViewController.swift
@@ -7,87 +7,176 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate {
-
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var carouselCV: UICollectionView!
+  
+  var carouselList:[CarouselDataModel] = []
+  
+  override func viewDidLoad() {
+    initData()
+    registerXib()
+    makeDelegate()
+    addCollectionView()
+  }
+  
+  
+  func addCollectionView(){
     
-    let screenSize = UIScreen.main.bounds
+    let layout = CarouselLayout()
     
-    @IBOutlet weak var carouselCV: UICollectionView!{
-        didSet {
-            let collectionViewLayout = CarouselLayout()
-
-
-            carouselCV.delegate = self
-            carouselCV.dataSource = self
-            carouselCV.collectionViewLayout = collectionViewLayout
-            carouselCV.backgroundColor = .clear
-            carouselCV.alpha = 0
-                    
-            carouselCV.contentInsetAdjustmentBehavior = .never
-            let cellWidth: CGFloat = floor(screenSize.width * (263/375))
-            let insetX = (carouselCV.bounds.width - cellWidth) / 2.0
-                    
-            carouselCV.contentInset = UIEdgeInsets(top: 0, left: insetX, bottom: 0, right: insetX)
-            carouselCV.decelerationRate = .normal
-                    
-                }
-            }
+    // 현재 페이지의 크기 비율을 지정, 피그마 참고했습니다.
+    let centerItemWidthScale: CGFloat = 327/375
+    let centerItemHeightScale: CGFloat = 1
     
+    layout.itemSize = CGSize(width: carouselCV.frame.size.width*centerItemWidthScale, height: carouselCV.frame.size.height*centerItemHeightScale)
     
-    var carouselList:[CarouselDataModel] = []
+    layout.sideItemScale = 464/520
+    layout.spacing = 12
+    layout.sideItemAlpha = 0.4
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        initData()
-        registerXib()
-        makeDelegate()
-        
-//        let flowLayout: CarouselLayout  // 이 타입의
-//            flowLayout = CarouselLayout()  // 인스턴스 생성
-//
-//            self.carouselCV.collectionViewLayout = flowLayout
-    }
-    
-    func makeDelegate() {
-//        carouselCV.delegate = self
-//        carouselCV.dataSource = self
-    }
-    
-    func registerXib() {
-        let xibName = UINib(nibName: CarouselCVC.identifier, bundle: nil)
-        carouselCV.register(xibName, forCellWithReuseIdentifier: CarouselCVC.identifier)
-    }
-    
-    func initData(){
-        carouselList.append(contentsOf: [
-            CarouselDataModel(ImageName: "grey"),
-            CarouselDataModel(ImageName: "red"),
-            CarouselDataModel(ImageName: "yellow"),
-            CarouselDataModel(ImageName: "green"),
-            CarouselDataModel(ImageName: "blue")
-        ])
-    }
-    
-    //delegate 등록 해주기..!
-
-
+    carouselCV.collectionViewLayout = layout
+  }
+  
+  func makeDelegate() {
+//    carouselCV.delegate = self
+    carouselCV.dataSource = self
+  }
+  
+  func registerXib() {
+    let xibName = UINib(nibName: CarouselCVC.identifier, bundle: nil)
+    carouselCV.register(xibName, forCellWithReuseIdentifier: CarouselCVC.identifier)
+  }
+  
+  func initData(){
+    carouselList.append(contentsOf: [
+      CarouselDataModel(ImageName: "grey"),
+      CarouselDataModel(ImageName: "red"),
+      CarouselDataModel(ImageName: "yellow"),
+      CarouselDataModel(ImageName: "green"),
+      CarouselDataModel(ImageName: "blue")
+    ])
+  }
+  
 }
 
 extension ViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
-    }
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  //indexPath에 어떤 cell 데이터를 넣을 지 결정하는 메소드
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CarouselCVC.identifier, for: indexPath) as? CarouselCVC else {return UICollectionViewCell()}
     
-    //indexPath에 어떤 cell 데이터를 넣을 지 결정하는 메소드
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CarouselCVC.identifier, for: indexPath) as? CarouselCVC else {return UICollectionViewCell()}
-
-        cell.setData(appData: carouselList[indexPath.row])
-        return cell
-    }
+    cell.setData(appData: carouselList[indexPath.row])
+    return cell
+  }
 }
+
+//extension ViewController: UICollectionViewDelegateFlowLayout {
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//    let screenWidth = UIScreen.main.bounds.width
+//    let cellWidth = screenWidth
+//    let cellHeight = cellWidth * (200/375)
+//    return CGSize(width: cellWidth, height: cellHeight)
+//
+//  }
+//
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//    UIEdgeInsets.zero
+//  }
+//
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//    12
+//  }
+//
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//    12
+//  }
+//}
+
+
+
+
+
+
+
+//--- 여기는 예전에 쓴 코드---//
+//    @IBOutlet weak var carouselCV: UICollectionView!{
+//        didSet {
+//            let collectionViewLayout = CarouselLayout()
+//
+//
+//            carouselCV.delegate = self
+//            carouselCV.dataSource = self
+//            carouselCV.collectionViewLayout = collectionViewLayout
+//            carouselCV.backgroundColor = .clear
+//            carouselCV.alpha = 0
+//
+//            carouselCV.contentInsetAdjustmentBehavior = .never
+//            let cellWidth: CGFloat = floor(screenSize.width * (263/375))
+//            let insetX = (carouselCV.bounds.width - cellWidth) / 2.0
+//
+//            carouselCV.contentInset = UIEdgeInsets(top: 0, left: insetX, bottom: 0, right: insetX)
+//            carouselCV.decelerationRate = .normal
+//
+//                }
+//            }
+//
+//
+//    var carouselList:[CarouselDataModel] = []
+//
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        initData()
+//        registerXib()
+//        makeDelegate()
+//
+////        let flowLayout: CarouselLayout  // 이 타입의
+////            flowLayout = CarouselLayout()  // 인스턴스 생성
+////
+////            self.carouselCV.collectionViewLayout = flowLayout
+//    }
+//
+//    func makeDelegate() {
+////        carouselCV.delegate = self
+////        carouselCV.dataSource = self
+//    }
+//
+//    func registerXib() {
+//        let xibName = UINib(nibName: CarouselCVC.identifier, bundle: nil)
+//        carouselCV.register(xibName, forCellWithReuseIdentifier: CarouselCVC.identifier)
+//    }
+//
+//    func initData(){
+//        carouselList.append(contentsOf: [
+//            CarouselDataModel(ImageName: "grey"),
+//            CarouselDataModel(ImageName: "red"),
+//            CarouselDataModel(ImageName: "yellow"),
+//            CarouselDataModel(ImageName: "green"),
+//            CarouselDataModel(ImageName: "blue")
+//        ])
+//    }
+//
+//    //delegate 등록 해주기..!
+//}
+//
+//extension ViewController: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 5
+//    }
+//
+//    //indexPath에 어떤 cell 데이터를 넣을 지 결정하는 메소드
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CarouselCVC.identifier, for: indexPath) as? CarouselCVC else {return UICollectionViewCell()}
+//
+//        cell.setData(appData: carouselList[indexPath.row])
+//        return cell
+//    }
+//}
 
 //extension ViewController: UICollectionViewDelegateFlowLayout {
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
